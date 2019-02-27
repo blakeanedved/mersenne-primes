395fd3ce284dea89790d323a22eaf26da7755b37
blakeanedved <blakeanedved@gmail.com>
2019-02-26 21:26:38 -0600

    Implemented a threaded way to find the next prime

--- main.cpp
+++ main.cpp
@@ -1,31 +1,86 @@
 #include <iostream>
+#include <thread>
 #include <boost/multiprecision/gmp.hpp>
 namespace mp = boost::multiprecision;
 
-auto nextLucasLehmer(mp::mpz_int) -> mp::mpz_int;
+#define NUM_THREADS 8
+
+std::vector<bool> isFinished(NUM_THREADS, true);
+std::vector<bool> foundPrime(NUM_THREADS, false);
+
+std::vector<mp::mpz_int> M(NUM_THREADS, 0);
+std::vector<mp::mpz_int> L(NUM_THREADS, 0);
+
+inline auto nextLucasLehmer(mp::mpz_int) -> mp::mpz_int;
+auto checkMersenneForPrimeThreaded(int, int) -> void;
+auto checkMersenneForPrime(int) -> bool;
 
 auto main() -> int {
 	
-	int mersenneExp;
-
-	std::cout << "What n would you like to check for primality (2^n-1)? >> ";
-	std::cin >> mersenneExp;
-	mp::mpz_int mersenneNum = mp::pow(mp::mpz_int(2), mersenneExp) - 1;
-	mp::mpz_int lucasLehmerNum = 4;
-
-	for (int i = 1; i < mersenneExp - 1; i++){
-		lucasLehmerNum = nextLucasLehmer(lucasLehmerNum) % mersenneNum;
+	int currentNum = 11000;
+	currentNum--;
+	bool searching = true;
+	int foundIndex;
+	std::vector<int> nums;
+	std::vector<std::thread> threads;
+	std::vector<bool> done(NUM_THREADS, false);
+	
+	for (int i = 0; i < NUM_THREADS; i++){
+		nums.push_back(++currentNum);
+		threads.push_back(std::thread(checkMersenneForPrimeThreaded, currentNum, i));
 	}
 
-	if (lucasLehmerNum % mersenneNum == 0){
-		std::cout << "2^" << mersenneExp << "-1 is prime" << std::endl;
-	} else {
-		std::cout << "2^" << mersenneExp << "-1 is not prime" << std::endl;
+	while (searching){
+		for (int i = 0; i < NUM_THREADS; i++){
+			if (isFinished[i]){
+				threads[i].join();
+				if (foundPrime[i]){
+					std::cout << "Found a prime number at 2^" << nums[i] << "-1" << std::endl;
+					searching = false;
+					foundIndex = i;
+				}
+				if (searching){
+					nums[i] = ++currentNum;
+					threads[i] = std::thread(checkMersenneForPrimeThreaded, currentNum, i);
+					isFinished[i] = false;
+				}
+			}
+		}
 	}
+
+	for (int i = 0; i < NUM_THREADS; i++)
+		if (i != foundIndex)
+			threads[i].join();
 		
 	return 0;
 }
 
 inline auto nextLucasLehmer(mp::mpz_int a) -> mp::mpz_int {
 	return (a * a) - 2;
+}
+
+auto checkMersenneForPrimeThreaded(int p, int my_id) -> void {
+	M[my_id] = mp::pow(mp::mpz_int(2), p) - 1;
+	L[my_id] = 4;
+
+	for (int i = 1; i < p - 1; i++){
+		L[my_id] = nextLucasLehmer(L[my_id]) % M[my_id];
+	}
+
+	if (L[my_id] % M[my_id] == 0){
+		foundPrime[my_id] = true;
+	}
+
+	isFinished[my_id] = true;
+}
+
+auto checkMersenneForPrime(int p) -> bool {
+	mp::mpz_int mersenneNum = mp::pow(mp::mpz_int(2), p) - 1;
+	mp::mpz_int lucasLehmerNum = 4;
+
+	for (int i = 1; i < p - 1; i++){
+		lucasLehmerNum = nextLucasLehmer(lucasLehmerNum) % mersenneNum;
+	}
+
+	return lucasLehmerNum % mersenneNum == 0;
 }
